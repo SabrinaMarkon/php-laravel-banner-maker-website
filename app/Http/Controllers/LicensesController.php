5a66c590305f9ed51c62cd815ad0f5d024e8e5c9
Sabrina Markon <sabrina@phpsitescripts.com>
2016-12-11 17:41:33 -0700

    Members area license view added downline builder Paypal buttons

--- app/Http/Controllers/LicensesController.php
+++ app/Http/Controllers/LicensesController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\License;
-use App\Models\LicenseDLB;
+use App\Models\LicenseDLBSilver;
+use App\Models\LicenseDLBGold;
 use App\Models\Page;
 use App\Http\Controllers\Controller;
 use Session;
@@ -21,7 +22,7 @@ class LicensesController extends Controller
      * @return Response
      */
     public function index() {
-        // find out of the user has a current license.
+        // find out of the user has a current banner watermark license.
         $license = License::where('userid', '=', Session::get('user')->userid)->where('licenseenddate', '>=', new DateTime('now'))->orderBy('id', 'desc')->first();
         if ($license) {
             $licenseenddate = new DateTime($license->licenseenddate);
@@ -29,9 +30,30 @@ class LicensesController extends Controller
         } else {
             $licenseenddate = '';
         }
+
+        // find out if the user has a current gold downline builder license:
+        $licensedlbgold = LicenseDLBGold::where('userid', '=', Session::get('user')->userid)->where('licenseenddate', '>=', new DateTime('now'))->orderBy('id', 'desc')->first();
+        if ($licensedlbgold) {
+            $licensedlbgoldenddate = new DateTime($licensedlbgold->licenseenddate);
+            $licensedlbgoldenddate = $licensedlbgoldenddate->format('Y-m-d');
+        } else {
+            $licensedlbgoldenddate = '';
+        }
+
+        // find out if the user has a current silver downline builder license:
+        $licensedlbsilver = LicenseDLBSilver::where('userid', '=', Session::get('user')->userid)->where('licenseenddate', '>=', new DateTime('now'))->orderBy('id', 'desc')->first();
+        if ($licensedlbsilver) {
+            $licensedlbsilverenddate = new DateTime($licensedlbsilver->licenseenddate);
+            $licensedlbsilverenddate = $licensedlbsilverenddate->format('Y-m-d');
+        } else {
+            $licensedlbsilverenddate = '';
+        }
+
+        // in the view, if the user has an active gold license, no order button. If the user has no gold but has an active silver license, upgrade order button. Otherwise, full order button.
+
         // get the admin's content for the license sales page.
         $page = Page::where('slug', '=', 'license')->first();
-        return view('pages.license', compact('page', 'licenseenddate'));
+        return view('pages.license', compact('page', 'licenseenddate', 'licensedlbgoldenddate', 'licensedlbsilverenddate'));
     }
 
 }
--- resources/views/pages/license.blade.php
+++ resources/views/pages/license.blade.php
@@ -74,11 +74,128 @@
                         {{ Form::hidden('on0', 'User ID') }}
                         {{ Form::hidden('os0', Session::get('user')->userid) }}
                         {{ Form::hidden('notify_url', $domain . '/ipn') }}
-                        {{ Form::submit('Buy Banner License - ' . $licenseprice . ' ' . $licensepriceinterval, array('class' => 'btn btn-custom')) }}
+                        {{ Form::submit('Buy White Label Banner License - ' . $licenseprice . ' ' . $licensepriceinterval, array('class' => 'btn btn-custom')) }}
                         {{ Form::close() }}
                         @endif
                     </div>
                 </div>
+
+            <div class="form-group row">
+                <div class="col-sm-12"> <br>
+                    @if($licensedlbgoldenddate)
+                        {{-- Has highest level gold license already--}}
+                        <br><div class="alert alert-info">You already have an active Gold license that is good until {{ $licensedlbgoldenddate }}!</div>
+                    @elseif($licensedlbsilverenddate)
+                        {{-- Has medium level silver license --}}
+                        <br><div class="alert alert-info">You already have an active Silver license that is good until {{ $licensedlbsilverenddate }}!<br>
+                        BUT you can still upgrade to a Gold license to get an UNLIMITED number of your own programs added to the builder!</div>
+                        {{--GOLD--}}
+                        {{ Form::open(array('url' => 'https://www.paypal.com/cgi-bin/webscr', 'method' => 'POST', 'class' => 'form-horizontal form-page-small')) }}
+                        @if ($licensedlbgoldpriceinterval === 'lifetime')
+                            {{ Form::hidden('amount', $licensedlbgoldprice) }}
+                            {{ Form::hidden('cmd', '_xclick') }}
+                        @else
+                            {{ Form::hidden('a3', $licensedlbgoldprice) }}
+                            {{ Form::hidden('cmd', '_xclick-subscriptions') }}
+                            {{ Form::hidden('p3', '1') }}
+                            @if($licensedlbgoldpriceinterval === 'monthly')
+                                {{ Form::hidden('t3', 'M') }}
+                            @else
+                                {{ Form::hidden('t3', 'Y') }}
+                            @endif
+                            {{ Form::hidden('src', '1') }}
+                            {{ Form::hidden('sra', '1') }}
+                        @endif
+                        {{ Form::hidden('business', $adminpaypal) }}
+                        {{ Form::hidden('item_name', $sitename . ' - Gold Downline Builder License Upgrade') }}
+                        {{ Form::hidden('item_number', $licensedlbgoldpriceinterval) }}
+                        {{ Form::hidden('no_note', '1') }}
+                        {{ Form::hidden('page_style', 'PayPal') }}
+                        {{ Form::hidden('no_shipping', '1') }}
+                        {{ Form::hidden('return', $domain . '/thankyou') }}
+                        {{ Form::hidden('cancel', $domain . '/license') }}
+                        {{ Form::hidden('currency_code', 'USD') }}
+                        {{ Form::hidden('lc', 'US') }}
+                        {{ Form::hidden('bn', 'PP-BuyNowBF') }}
+                        {{ Form::hidden('on0', 'User ID') }}
+                        {{ Form::hidden('os0', Session::get('user')->userid) }}
+                        {{ Form::hidden('notify_url', $domain . '/ipn') }}
+                        {{ Form::submit('Buy Gold Downline Builder License Upgrade - ' . $licensedlbgoldprice . ' ' . $licensedlbgoldpriceinterval, array('class' => 'btn btn-custom')) }}
+                        {{ Form::close() }}
+
+                    @else
+                        {{-- Has no license yet need both license order buttons --}}
+
+                        {{--SILVER--}}
+                        {{ Form::open(array('url' => 'https://www.paypal.com/cgi-bin/webscr', 'method' => 'POST', 'class' => 'form-horizontal form-page-small')) }}
+                        @if ($licensedlbsilverpriceinterval === 'lifetime')
+                            {{ Form::hidden('amount', $licensedlbsilverprice) }}
+                            {{ Form::hidden('cmd', '_xclick') }}
+                        @else
+                            {{ Form::hidden('a3', $licensedlbsilverprice) }}
+                            {{ Form::hidden('cmd', '_xclick-subscriptions') }}
+                            {{ Form::hidden('p3', '1') }}
+                            @if($licensedlbsilverpriceinterval === 'monthly')
+                                {{ Form::hidden('t3', 'M') }}
+                            @else
+                                {{ Form::hidden('t3', 'Y') }}
+                            @endif
+                            {{ Form::hidden('src', '1') }}
+                            {{ Form::hidden('sra', '1') }}
+                        @endif
+                        {{ Form::hidden('business', $adminpaypal) }}
+                        {{ Form::hidden('item_name', $sitename . ' - Silver Downline Builder License') }}
+                        {{ Form::hidden('item_number', $licensedlbsilverpriceinterval) }}
+                        {{ Form::hidden('no_note', '1') }}
+                        {{ Form::hidden('page_style', 'PayPal') }}
+                        {{ Form::hidden('no_shipping', '1') }}
+                        {{ Form::hidden('return', $domain . '/thankyou') }}
+                        {{ Form::hidden('cancel', $domain . '/license') }}
+                        {{ Form::hidden('currency_code', 'USD') }}
+                        {{ Form::hidden('lc', 'US') }}
+                        {{ Form::hidden('bn', 'PP-BuyNowBF') }}
+                        {{ Form::hidden('on0', 'User ID') }}
+                        {{ Form::hidden('os0', Session::get('user')->userid) }}
+                        {{ Form::hidden('notify_url', $domain . '/ipn') }}
+                        {{ Form::submit('Buy Silver Downline Builder License - ' . $licensedlbsilverprice . ' ' . $licensedlbsilverpriceinterval, array('class' => 'btn btn-custom')) }}
+                        {{ Form::close() }}
+
+                        {{--GOLD--}}
+                        {{ Form::open(array('url' => 'https://www.paypal.com/cgi-bin/webscr', 'method' => 'POST', 'class' => 'form-horizontal form-page-small')) }}
+                        @if ($licensedlbgoldpriceinterval === 'lifetime')
+                            {{ Form::hidden('amount', $licensedlbgoldprice) }}
+                            {{ Form::hidden('cmd', '_xclick') }}
+                        @else
+                            {{ Form::hidden('a3', $licensedlbgoldprice) }}
+                            {{ Form::hidden('cmd', '_xclick-subscriptions') }}
+                            {{ Form::hidden('p3', '1') }}
+                            @if($licensedlbgoldpriceinterval === 'monthly')
+                                {{ Form::hidden('t3', 'M') }}
+                            @else
+                                {{ Form::hidden('t3', 'Y') }}
+                            @endif
+                            {{ Form::hidden('src', '1') }}
+                            {{ Form::hidden('sra', '1') }}
+                        @endif
+                        {{ Form::hidden('business', $adminpaypal) }}
+                        {{ Form::hidden('item_name', $sitename . ' - Gold Downline Builder License') }}
+                        {{ Form::hidden('item_number', $licensedlbgoldpriceinterval) }}
+                        {{ Form::hidden('no_note', '1') }}
+                        {{ Form::hidden('page_style', 'PayPal') }}
+                        {{ Form::hidden('no_shipping', '1') }}
+                        {{ Form::hidden('return', $domain . '/thankyou') }}
+                        {{ Form::hidden('cancel', $domain . '/license') }}
+                        {{ Form::hidden('currency_code', 'USD') }}
+                        {{ Form::hidden('lc', 'US') }}
+                        {{ Form::hidden('bn', 'PP-BuyNowBF') }}
+                        {{ Form::hidden('on0', 'User ID') }}
+                        {{ Form::hidden('os0', Session::get('user')->userid) }}
+                        {{ Form::hidden('notify_url', $domain . '/ipn') }}
+                        {{ Form::submit('Buy Gold Downline Builder License - ' . $licensedlbgoldprice . ' ' . $licensedlbgoldpriceinterval, array('class' => 'btn btn-custom')) }}
+                        {{ Form::close() }}
+                    @endif
+                </div>
+            </div>
 
         </div>
 
