a2e8e104868d55e912174f32016c890e2bbabf3d
Sabrina Markon <sabrina@phpsitescripts.com>
2016-12-17 15:32:52 -0700

    Add payza and paypal fields to members

---
+++ database/migrations/2016_12_17_221314_add_paypal_and_payza_columns_to_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPaypalAndPayzaColumnsToMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->string('paypal', 255)->nullable();
+            $table->string('payza', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            //
+        });
+    }
+}
